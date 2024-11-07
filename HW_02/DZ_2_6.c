599ed227d3e3a3985aea462d00de6493401ed939
Aleksey Bocharov <boch_a@mail.ru>
2024-11-07 22:49:55 +0300

    DZ2

--- HW_02/DZ_2_6.c
+++ HW_02/DZ_2_6.c
@@ -3,17 +3,17 @@
 
 int main()  // Таблица истинности для упрощённого выражения
 {
-	int A = 0;	int B = 0;	int C = 0; 
+    int A = 0;	int B = 0;	int C = 0; 
     int arr_A[8] = {0,0,0,0,1,1,1,1}; 
     int arr_B[8] = {0,0,1,1,0,0,1,1};
     int arr_C[8] = {0,1,0,1,0,1,0,1};
 
-	for (int i=0; i<8; i++) {
+    for (int i=0; i<8; i++) {
 	    
-		A = arr_A[i]; B = arr_B[i]; C = arr_C[i];
+        A = arr_A[i]; B = arr_B[i]; C = arr_C[i];
 		
-		printf("%d%d%d%d%d\n", A, B, C,  (!B||A) && (!(A||B)) && (!A||C),  !B&&!A  );
+        printf("%d%d%d%d%d\n", A, B, C,  (!B||A) && (!(A||B)) && (!A||C),  !B&&!A  );
 
-	} //for
-	return 0;
+    } //for
+    return 0;
 }
